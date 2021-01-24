410895bbcf038c2151a8c925f1ba8ae9f16448c0
SZLiu <38514287+qq491577803@users.noreply.github.com>
2021-01-25 00:31:10 +0800

    Create nonLocalMeanDn.h

---
+++ nonLocalMeanDn.h
@@ -0,0 +1,111 @@
+#pragma once
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<math.h>
+#include<time.h>
+#include<vector>
+#include"nonLocalMeanDn.h"
+using namespace cv;
+using namespace std;
+
+struct imageInfo{
+	int rows;
+	int cols;
+	cv::Mat srcImgRgb;
+	cv::Mat srcImgGray;
+	cv::Mat dstImgGray;
+};
+
+struct dnInfo{
+	int curWin;//7
+	int curWinRadius;//3
+	int searchWin;//21
+	int serachWinRadius;//10
+	float sigma;//16
+};
+
+class nonLocalMean{
+private: 
+	imageInfo imageInfo;
+	dnInfo dnInfo;
+	void imageShow(cv::Mat image);
+	void imageShow(cv::Mat src, cv::Mat dst);
+public:
+	void initInfo(char *imgPath, int curWin, int searchWin,int sigma);
+	void processor();
+	void calcPSNR();
+};
+
+void nonLocalMean::initInfo(char *imgPath,int curWin,int searchWin,int sigma){
+	imageInfo.srcImgRgb = cv::imread(imgPath);
+	imageInfo.rows = imageInfo.rows;
+	imageInfo.cols = imageInfo.cols;
+	cvtColor(imageInfo.srcImgRgb, imageInfo.srcImgGray,COLOR_BGR2GRAY);
+	imageInfo.dstImgGray = imageInfo.srcImgGray.clone();
+	imageInfo.dstImgGray.convertTo(imageInfo.dstImgGray,CV_32F);
+	dnInfo.curWin = curWin;//7
+	dnInfo.curWinRadius = curWin >> 1;//3
+	dnInfo.searchWin = searchWin;//21
+	dnInfo.serachWinRadius = searchWin >> 1;//10
+	dnInfo.sigma = sigma;
+}
+
+void nonLocalMean::imageShow(cv::Mat image){
+	cv::imshow("image",image);
+	cv::waitKey(0);
+}
+
+void nonLocalMean::imageShow(cv::Mat src,cv::Mat dst){
+	cv::imshow("srcImage", src);
+	cv::imshow("dstImage", dst);
+	cv::waitKey(0);
+}
+
+void nonLocalMean::processor(){
+
+
+	cv::Mat tmp;
+
+	for (int row = dnInfo.serachWinRadius; row < imageInfo.rows - dnInfo.serachWinRadius; row ++)
+	{
+		for (int col = dnInfo.serachWinRadius; col < imageInfo.cols - dnInfo.serachWinRadius; col++)
+		{
+			int bRow = row - dnInfo.serachWinRadius;//row - 10
+			int bCol = col - dnInfo.serachWinRadius;//col - 10
+			cv::Mat curArea = imageInfo.srcImgGray(cv::Rect(row - dnInfo.curWinRadius, col - dnInfo.curWinRadius,dnInfo.curWin,dnInfo.curWin));
+			float vSum = 0.0f;
+			float wtSum = 0.0f;
+			float tmp1 = 0.0f;
+			for(int subRow = bRow;subRow <= (bRow + dnInfo.curWinRadius + dnInfo.serachWinRadius + 1);subRow ++ )
+				for (int subCol = bCol; subCol <= (bCol + dnInfo.curWinRadius + dnInfo.serachWinRadius + 1); subCol++)
+				{
+					cv::Mat searchArea = imageInfo.srcImgGray(cv::Rect(subRow, subCol, dnInfo.curWin, dnInfo.curWin));
+					tmp = searchArea - curArea;
+					tmp1 = tmp.dot(tmp);
+					tmp1 = exp(- tmp1 / (dnInfo.sigma * dnInfo.sigma));
+					wtSum += tmp1;
+					
+
+					
+
+
+				}
+			
+
+		}
+	}
+}
+
+void nonLocalMean::calcPSNR(){
+
+}
+
+
+
+void runNonLocalMean(){
+	char imgPath[256] = "./data/noiseImage.jpeg";
+	nonLocalMean nonLocalMean;
+	nonLocalMean.initInfo(imgPath,7,21,16);
+
+}
+
